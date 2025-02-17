0f19a03e3b146e0443b571187ee9b1535cc4efc2
Mitalo4 <129074436+Mitalo4@users.noreply.github.com>
2025-02-17 03:34:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -72,4 +72,4 @@ def buscar():
     return jsonify({"data": datetime.today().strftime("%d-%m-%Y"), "links_encontrados": links})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=5000)
